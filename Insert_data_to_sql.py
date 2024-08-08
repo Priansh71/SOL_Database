595574d1b301b56b7b62067337264bade29523d2
Priansh71 <90593472+Priansh71@users.noreply.github.com>
2024-08-08 15:35:12 +0530

    Update Insert_data_to_sql.py

--- Insert_data_to_sql.py
+++ Insert_data_to_sql.py
@@ -1,1 +1,13 @@
+import sqlite3
 
+con = sqlite3.connect('database.db')
+cur = con.cursor()
+
+new_rows= [('100.01.01.09','abc.com',100),
+           ('100.10.10.12','xyz.com',102)]
+
+cur.executemany("insert into 'ips' values(?,?,?),new_rows")
+con.commit()
+
+cur.execute("select * from 'ips'")
+print(cur.fetchall())
