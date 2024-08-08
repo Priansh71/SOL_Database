9a1013490871bbea7a08d82e1689f74fc7db7f02
Priansh71 <90593472+Priansh71@users.noreply.github.com>
2024-08-08 13:02:23 +0530

    SQL_to_CSV.py

--- Convert SQL data to CSV.py
+++ Convert SQL data to CSV.py
@@ -1,1 +1,14 @@
+import sqlite3 
+import pandas as pd 
 
+
+con = sqlite3.connect('database.db')
+cur = con.cursor()
+
+pandas.read_sql_query("SELECT * FROM 'IPS' ORDER BY asn" , con) # con is the object to specify which connection is specified
+print(df)
+
+df.to_csv('database.csv',index=NONE)
+# Before this install 
+# pip install openpyxl
+df.to_excel('database.xlsx',index=NONE)
